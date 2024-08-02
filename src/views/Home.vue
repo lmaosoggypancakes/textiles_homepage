150e5612868ef21871a5730a77ecfbb0d8a89615
coder6583 <thedude6583@gmail.com>
2024-08-02 02:25:00 -0400

    draws footprints

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
   <main class="h-full flex flex-col items-center justify-center">
-    <div class="border-2 rounded-lg p-8 border-foam flex flex-col justify-center items-center hover:bg-surface transition-all cursor-pointer" @click="startFileUpload" :class="loading && 'hover:cursor-not-allowed hover:border-surface hover:bg-base border-surface'">
-      <FolderArrowDownIcon  class="w-24 h-24 text-text" v-if="!loading"/>
+    <div
+      class="border-2 rounded-lg p-8 border-foam flex flex-col justify-center items-center hover:bg-surface transition-all cursor-pointer"
+      @click="startFileUpload"
+      :class="
+        loading &&
+        'hover:cursor-not-allowed hover:border-surface hover:bg-base border-surface'
+      "
+    >
+      <FolderArrowDownIcon class="w-24 h-24 text-text" v-if="!loading" />
       <span v-if="!loading" class="text-text">Upload your netlist file</span>
       <ArrowPathIcon class="w-24 h-24 animate-spin text-text" v-else />
-      <input type="file" class="hidden" ref="fileUpload" @change="filePicked"/>
+      <input type="file" class="hidden" ref="fileUpload" @change="filePicked" />
       <pre class="bg-base">{{ fileName }}</pre>
     </div>
     <span>{{ statusMessage }}</span>
@@ -12,72 +19,78 @@
 </template>
 
 <script setup lang="ts">
-import { FolderArrowDownIcon, ArrowPathIcon } from "@heroicons/vue/24/outline"
+import { FolderArrowDownIcon, ArrowPathIcon } from "@heroicons/vue/24/outline";
 import { ref } from "vue";
-import { type Netlist } from "@/../types"
-import axios, { AxiosError, type AxiosResponse } from "axios"
+import { type Netlist } from "@/../types";
+import axios, { AxiosError, type AxiosResponse } from "axios";
 import { useRouter } from "vue-router";
 
-const router = useRouter()
+const router = useRouter();
 
-const loading = ref(false)
-const fileName = ref("")
-const statusMessage = ref("")
-const fileUpload = ref<HTMLInputElement | null>(null)
+const loading = ref(false);
+const fileName = ref("");
+const statusMessage = ref("");
+const fileUpload = ref<HTMLInputElement | null>(null);
 
 const startFileUpload = () => {
   if (fileUpload.value && !loading.value) {
-    fileUpload.value.click()
+    fileUpload.value.click();
   }
-}
+};
 
 const filePicked = (event: Event) => {
-  loading.value = true
-  const target = event.target as HTMLInputElement
+  loading.value = true;
+  const target = event.target as HTMLInputElement;
   if (target == null || target.files == null) {
-    alert("Error: bad event")
-    return
+    alert("Error: bad event");
+    return;
   }
-  const file = target.files[0]
-  fileName.value = file.name
-  console.log(file)
-  const reader = new FileReader()
-  statusMessage.value = "reading file..."
+  const file = target.files[0];
+  fileName.value = file.name;
+  console.log(file);
+  const reader = new FileReader();
+  statusMessage.value = "reading file...";
   reader.readAsText(file);
 
   reader.onload = async () => {
-    statusMessage.value = ""
+    statusMessage.value = "";
     const data = reader.result;
-    loading.value = false
+    loading.value = false;
     if (data == null) {
-      alert("data is null. tell josef pls")
+      alert("data is null. tell josef pls");
       return;
     }
-    await parseFile(data as string)
+    await parseFile(data as string);
   };
 
   reader.onerror = () => {
     console.log(reader.error);
   };
-}
+};
 
 const parseFile = async (data: string) => {
-  statusMessage.value = "parsing and verifying netlist..."
+  statusMessage.value = "parsing and verifying netlist...";
   // https://api.stretchy.soggypancakes.tech/parse
-  const response: void | AxiosResponse = await axios.post("http://localhost:8000/parse", {
-    data
-  }).catch((reason: AxiosError) => {
-    console.log(reason)
-    if(reason.response?.status == 400) {
-      statusMessage.value = (reason.response?.data as any).detail || "unknown error. tell josef pls :("; // i hate this
-    }
-    if (reason.response?.status == 500) {
-      statusMessage.value = "Internal server error </3"
-    }
-  })
-  if(response) {
-    const netlist = response.data
-    router.push(`/session/?netlist=${encodeURIComponent(JSON.stringify(netlist))}`)
+  const response: void | AxiosResponse = await axios
+    .post("http://localhost:8000/parse", {
+      data,
+    })
+    .catch((reason: AxiosError) => {
+      console.log(reason);
+      if (reason.response?.status == 400) {
+        statusMessage.value =
+          (reason.response?.data as any).detail ||
+          "unknown error. tell josef pls :("; // i hate this
+      }
+      if (reason.response?.status == 500) {
+        statusMessage.value = "Internal server error </3";
+      }
+    });
+  if (response) {
+    const netlist = response.data;
+    router.push(
+      `/session/?netlist=${encodeURIComponent(JSON.stringify(netlist))}`
+    );
   }
-}
+};
 </script>
--- types.ts
+++ types.ts
@@ -1,64 +1,70 @@
 // a netlist type according to kinparse, reduced to assume only 1 sheet
 export interface Netlist {
-    libraries: {
-        name: string;
-        uri: string;
-    }[],
-    parts: {
-        ref: string;
-        value: string;
-        name: string;
-    }[],
-    nets: {
-        name: string;
-        code: number;
-        pins: {
-            ref: string;
-            num: number
-        }[]
-    }[]
+  libraries: {
+    name: string;
+    uri: string;
+  }[];
+  parts: {
+    ref: string;
+    value: string;
+    name: string;
+    footprint: string;
+  }[];
+  nets: {
+    name: string;
+    code: number;
+    pins: {
+      ref: string;
+      num: number;
+    }[];
+  }[];
 }
 
 export interface Vector {
-    x: number;
-    y: number;
-    angle: number;
-    mag: number;
+  x: number;
+  y: number;
+  angle: number;
+  mag: number;
 }
 
 export interface PhysicalNode {
-    ref: string;
-    acc: Vector;
-    vel: Vector;
-    x: number;
-    y: number;
-    color: string;
+  ref: string;
+  acc: Vector;
+  vel: Vector;
+  x: number;
+  y: number;
+  color: string;
+  footprint: string;
 }
 
 export interface PhysicalConnection {
-    one: PhysicalNode;
-    two: PhysicalNode;
-    ref: string;
+  one: PhysicalNode;
+  two: PhysicalNode;
+  ref: string;
 }
 
 export interface Constraint {
-    // represents a constraint of a node during the simulation.
-    // during the simulation, any node that has a constraint
-    // cannot exit the shape defined by an array of point-tos
-    node: PhysicalNode;
-    shape: [number, number][]
-
+  // represents a constraint of a node during the simulation.
+  // during the simulation, any node that has a constraint
+  // cannot exit the shape defined by an array of point-tos
+  node: PhysicalNode;
+  shape: [number, number][];
 }
 
 export interface Breakout {
-    nodes: PhysicalNode[];
-    pins: 4 | 6 | 12 | 18;
-    ref: string;
+  nodes: PhysicalNode[];
+  pins: 4 | 6 | 12 | 18;
+  ref: string;
 }
 
 export interface BreakoutConnection {
-    connection: PhysicalConnection;
-    one: Breakout;
-    two: Breakout;
-    ref: string;
+  connection: PhysicalConnection;
+  one: Breakout;
+  two: Breakout;
+  ref: string;
+}
+
+export interface Footprint {
+  paths: [number, number][][][];
+  pins: number[][];
 }
